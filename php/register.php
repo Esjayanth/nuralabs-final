09cd14bb48dfd5960aae92e31e5c84ae56352d9f
Jai Bala Adithya <jbadithya2005@gmail.com>
2026-08-15 02:05:19 +0530

    Add debug logging for request method

--- php/register.php
+++ php/register.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/config.php';
-
+error_log('DEBUG REQUEST_METHOD: ' . ($_SERVER['REQUEST_METHOD'] ?? 'MISSING'));
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     jsonResponse(false, 'Method not allowed', [], 405);
 }
